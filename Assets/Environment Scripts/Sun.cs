e6f9c73581c10569e4cc035dc10467cca0760e45
JinLimJim <143099Q@mymail.nyp.edu.sg>
2016-11-08 20:48:21 +0800

    Sunlight, Moonlight, and Terrain
    Added in sun, moon and a rough sketch terrain.

---
+++ Assets/Environment Scripts/Sun.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sun : MonoBehaviour {
+
+	public float sunSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround (Vector3.zero, Vector3.right, sunSpeed * Time.deltaTime);
+		transform.LookAt (Vector3.zero);
+	}
+}
