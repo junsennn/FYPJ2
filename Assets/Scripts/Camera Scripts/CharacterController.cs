7625b4a233354528b77008c992acc5ac72dd1347
JinLimJim <143099Q@mymail.nyp.edu.sg>
2016-11-08 21:27:14 +0800

    Bin OBJ, camera
    Added in Rubbish bin asset
    Added in FPS Camera, and Player Control script
    Added a Prefab folder.

---
+++ Assets/Scripts/Camera Scripts/CharacterController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+
+	// This speed is for the character
+	public float speed;
+	public float jumpspeed;
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		// This is to lock the cursor within the game and to also disable the mouse cursor icon
+		Cursor.lockState = CursorLockMode.None;
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// Translate forward or backward
+		float translateForwardBack = Input.GetAxis ("Vertical") * speed; // "Vertical" can be found at Edit -> Project Settings -> Input
+
+		// Translate either left or right
+		float strafeLeftRight = Input.GetAxis ("Horizontal") * speed; // "Horizontal" can be found at Edit -> Project Settings -> Input
+
+		translateForwardBack *= Time.deltaTime;
+		strafeLeftRight *= Time.deltaTime;
+
+		transform.Translate (strafeLeftRight, 0.0f, translateForwardBack);
+
+		// This is to get the mouse cursor icon back and to unlock the cursor from the game
+		//if(Input.GetKeyDown("escape"))
+	//	{
+		//	Cursor.lockState = CursorLockMode.None;
+		//}
+
+		// This is to allow the character to jump up and then back down again
+	//	if(Input.GetKeyDown ("space"))
+	//	{
+		//	print ("I am Jumping!");
+		//	Vector3 jumping = new Vector3 (0, 20, 0);
+		////	rb.AddForce (jumping * jumpspeed);
+		//}
+	}
+}
---
+++ Assets/Scripts/Camera Scripts/FirstPersonCameraControl.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstPersonCameraControl : MonoBehaviour {
+
+	// 2D Vector for the camera's movements
+	Vector2 mouseLook;
+
+	// 2D Vector for the mouse to move smoothly
+	Vector2 smoothVector;
+
+	// Values for mouse sensitivity
+	public float sensitivity;
+
+	// Values for the mouse to move smoothly
+	public float smoothValues;
+	 
+	GameObject character;
+
+	// Use this for initialization
+	void Start () {
+		// Assigning the character to the camera's parent
+		character = this.transform.parent.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/* -> To get the mouse movement values
+		   -> var is used as it defines the type of the variable by the complier */
+		var mouseMoveValues = new Vector2 (Input.GetAxisRaw ("Mouse X"), Input.GetAxisRaw ("Mouse Y"));
+
+		// To increase the mouse movement values with sensitivity and smoothing
+		mouseMoveValues = Vector2.Scale (mouseMoveValues, new Vector2 (sensitivity * smoothValues, sensitivity * smoothValues));
+
+		// To smooth the mouse's x-axis, lerp is used to prevent the snapping of one's direction to the other. (Should be used more for movements?) 
+		smoothVector.x = Mathf.Lerp (smoothVector.x, mouseMoveValues.x, 1f / smoothValues);
+		// To smooth the mouse's y-axis
+		smoothVector.y = Mathf.Lerp (smoothVector.y, mouseMoveValues.y, 1f / smoothValues);
+	
+		// Add the smoothVector (smoothing of mouse) to the mouseLook
+		mouseLook += smoothVector;
+
+		// To clamp the mouse for looking up and down
+		mouseLook.y = Mathf.Clamp(mouseLook.y, -90.0f, 90.0f);
+
+		/* -> Giving the camera a rotation
+		   -> "-" added to the "mouseLook.y" for inverted movement of up and down
+		   -> Vector3.right is to rotate the camera around the y-axis */
+		transform.localRotation = Quaternion.AngleAxis (-mouseLook.y, Vector3.right);
+
+		// To use the x-axis of the mouse to rotate around the character's up
+		character.transform.localRotation = Quaternion.AngleAxis (mouseLook.x, character.transform.up);
+
+		// -Optional- Do not need the main camera to be the child of a GameObject
+		//transform.rotation = Quaternion.Euler(-mouseLook.y, mouseLook.x, 0.0f);
+	}
+}
