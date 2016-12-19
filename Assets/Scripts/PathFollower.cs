d7c8c0150cd38f9de2b2dae74ac50773d8186f1f
JinLimJim <143099Q@mymail.nyp.edu.sg>
2016-12-19 19:51:01 +0800

    Time Limit UI, Basic AI
    Added TIme Limit UI
    Added in Basic AI path

---
+++ Assets/Scripts/PathFollower.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PathFollower : MonoBehaviour {
+
+	public Transform[] path;
+	public float speed = 5.0f;
+	public float reachDist = 1.0f;
+	public int currentPoint = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float dist = Vector3.Distance(path [currentPoint].position, transform.position);
+
+		transform.position = Vector3.Lerp(transform.position, path[currentPoint].position, Time.deltaTime * speed);
+
+		if (dist <= reachDist) {
+			currentPoint++;
+		}
+
+		if (currentPoint >= path.Length) {
+			currentPoint = 0;
+		}
+	}
+
+	void onDrawGizmos() {
+		if (path.Length > 0) {
+			for (int i = 0; i < path.Length; i++) {
+				if (path [i] != null) {
+					Gizmos.DrawSphere (path [i].position, reachDist);
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Text Scripts/TimeLimitCounter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TimeLimitCounter : MonoBehaviour {
+
+	public double timer = 60.0;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.deltaTime;
+		GetComponent<Text> ().text = "Time Left: " + timer.ToString();
+	}
+}
