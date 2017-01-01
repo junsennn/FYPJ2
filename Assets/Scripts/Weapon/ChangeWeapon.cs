d2f8c0b584528d201d434c10aa703b9b0f4802eb
junsennn <ongjunsen@gmail.com>
2017-01-01 10:03:55 +0800

    Moved scripts to folder , changed abit of main menu

---
+++ Assets/Scripts/Weapon/ChangeWeapon.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI;
+
+public class ChangeWeapon : MonoBehaviour {
+
+    GameObject gunTransform;
+
+    public Text weapon_Name;
+
+	// Use this for initialization
+	void Start () {
+        
+        gunTransform = Instantiate(transform.GetChild(0).gameObject);
+        gunTransform.SetActive(false);
+        gunTransform.transform.parent = transform;
+        gunTransform.transform.position = transform.GetChild(0).position;
+        gunTransform.transform.rotation = transform.GetChild(0).rotation;
+
+        weapon_Name.text = transform.GetChild(0).name;
+    }
+	
+    void OnTriggerEnter(Collider col)
+    {
+        if (!transform.GetChild(0).gameObject.activeSelf && col.tag == "Weapon")
+        {
+            col.transform.parent = transform;
+            Destroy(col.transform.gameObject.GetComponent<Rigidbody>());
+            col.transform.SetAsFirstSibling();
+
+            col.transform.position = gunTransform.transform.position;
+            col.transform.rotation = gunTransform.transform.rotation;
+
+            weapon_Name.text = transform.GetChild(0).name;
+
+            if (transform.GetChild(0).name == "Portal gun")
+                transform.gameObject.AddComponent<ObjectGrab>();
+            if (transform.GetChild(0).name == "Zarya Gun")
+                transform.gameObject.AddComponent<ZaryaGun>();
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.G) && transform.GetChild(0).gameObject.activeSelf)
+        {
+            if (transform.GetChild(0).name == "Portal gun")
+                Destroy(GetComponent<ObjectGrab>());
+            if (transform.GetChild(0).name == "Zarya Gun")
+                Destroy(GetComponent<ZaryaGun>());
+
+            transform.GetChild(0).gameObject.AddComponent<Rigidbody>();
+            transform.GetChild(0).GetComponent<Rigidbody>().AddForce(transform.GetComponent<Camera>().transform.forward * 300);
+            transform.GetChild(0).parent = null;
+
+            weapon_Name.text = "No Weapon";
+        }
+
+	}
+}
---
+++ Assets/Scripts/Weapon/ObjectFollow.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectFollow : MonoBehaviour
+{
+
+    private GameObject player;
+    public float distance = 5.0f;
+    float speed = 300.0f;
+    Camera playerCamera;
+    float forceAmount = 20000;
+
+    private GameObject itemGrabbed;
+
+    private bool ZaryaGun = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.Find("Player").transform.GetChild(0).gameObject;
+        playerCamera = player.GetComponent<Camera>();
+
+        if (player.GetComponent<ObjectGrab>())
+            itemGrabbed = player.GetComponent<ObjectGrab>().itemGrabbed;
+        else if (player.GetComponent<ZaryaGun>())
+        {
+            itemGrabbed = player.GetComponent<ZaryaGun>().itemGrabbed;
+            ZaryaGun = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        #region Drop Item
+        if (itemGrabbed != null)
+        {
+            //if (Input.GetKeyDown(KeyCode.E))
+            if (!Input.GetMouseButton(0))
+            {
+                itemGrabbed = null;
+
+                if (ZaryaGun)
+                    player.GetComponent<ZaryaGun>().itemGrabbed = null;
+                else
+                    player.GetComponent<ObjectGrab>().itemGrabbed = null;
+
+                Destroy(this);
+            }
+
+            //if (Input.GetMouseButtonDown(0))
+            if (Input.GetMouseButtonDown(1))
+            {
+                GetComponent<Rigidbody>().AddForce(playerCamera.transform.forward * forceAmount);
+                itemGrabbed = null;
+
+                if (ZaryaGun)
+                    player.GetComponent<ZaryaGun>().itemGrabbed = null;
+                else
+                    player.GetComponent<ObjectGrab>().itemGrabbed = null;
+
+                Destroy(this);
+            }
+
+        }
+
+
+        #endregion
+
+        #region Scrolling
+        if (distance > 3.0f)
+            distance += Input.mouseScrollDelta.y;
+        if (distance < 3.0f)
+            distance = 3.1f;
+        #endregion
+
+        #region Moving
+        if (!ZaryaGun)
+            GetComponent<Rigidbody>().velocity = (((playerCamera.transform.position + playerCamera.transform.forward * distance) - transform.position) * speed * Time.deltaTime);
+        else
+            transform.position = playerCamera.transform.position + playerCamera.transform.forward * distance;
+        #endregion
+
+    }
+}
