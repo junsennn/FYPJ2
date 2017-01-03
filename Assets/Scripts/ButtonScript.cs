9e90664d08534a667df0b3baef75b1cea1b30242
JinLimJim <143099Q@mymail.nyp.edu.sg>
2017-01-03 23:16:00 +0800

    Sounds for Main Menu
    Sounds for Main Menu

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour {
+
+	public AudioSource sound;
+	private Button button;
+
+	// Use this for initialization
+	void Start () {
+		sound.playOnAwake = false;
+		button.GetComponent<Button> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		sound.Play ();
+	}
+		
+}
