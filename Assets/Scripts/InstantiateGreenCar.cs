23bcb6baf4ca10852bd5bfa8230ae3ec5739c0a8
JinLimJim <143099Q@mymail.nyp.edu.sg>
2017-01-05 22:16:46 +0800

    Complete design for Town Level (Day)
    - Design for the town level (day) is completed.
    - Navmesh baked for NPC route.
    - Collision added to the Models in the scene (except for Instantiated
    objects).

---
+++ Assets/Scripts/InstantiateGreenCar.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstantiateGreenCar : MonoBehaviour {
+
+	public GameObject thisPrefab;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < 1; i++) {
+			for (int j = 0; j < 4; j++) {
+				Instantiate (thisPrefab, new Vector3 ((i * 50.0f) + 20.0f, 36.0f, (j * 20.0f) - 90.0f), Quaternion.identity);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/InstantiateTrees.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstantiateTrees : MonoBehaviour {
+
+	public GameObject thisPrefab;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				Instantiate (thisPrefab, new Vector3 ((i * 50.0f) - 220.0f, 38.0f, (j * 20.0f) + 50.0f), Quaternion.Euler(-90, 0, 0));
+			}
+		}
+	}
+}
