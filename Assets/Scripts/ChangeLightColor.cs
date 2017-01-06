c2195dcdfbb7b9481a24e728b57f081ba99999b8
JinLimJim <143099Q@mymail.nyp.edu.sg>
2017-01-06 16:46:38 +0800

    Changed the walls and added lights for Tutorial Scene
    Changed the walls and added lights for Tutorial Scene

---
+++ Assets/Scripts/ChangeLightColor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeLightColor : MonoBehaviour {
+
+	public float durationTime;
+	private float thisTime;
+	public Color color0 = Color.red;
+	public Color color1 = Color.blue;
+	public Color color2 = Color.white;
+
+	public Light thisLight;
+
+	// Use this for initialization
+	void Start () {
+		thisLight = GetComponent<Light> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		thisTime = Mathf.PingPong (Time.time, durationTime) / durationTime;
+		thisLight.color = Color.Lerp (color0, color2, thisTime);
+	}
+}
---
+++ Assets/Scripts/FloatingObject.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloatingObject : MonoBehaviour {
+
+	private float originalY;
+	public float floatingStrength = 1; 
+
+	// Use this for initialization
+	void Start () {
+		this.originalY = this.transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (transform.position.x, originalY + ((float)Mathf.Sin (Time.time) * floatingStrength), transform.position.z);
+	}
+}
