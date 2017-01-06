fcc045db9dbcb57bc49f3cf5072d581bfeb2a8bb
junsennn <ongjunsen@gmail.com>
2017-01-06 19:15:39 +0800

    tried to do the level thing, not done yet

---
+++ Assets/Scripts/GameLevelSettings.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class GameLevelSettings : MonoBehaviour {
+   
+    private GameObject canvas;
+
+    public GameObject WinPanel;
+    public GameObject LosePanel;
+
+    public float TimeLimit;
+    private float TimeElapsed;
+
+    public int MinRubbishRequired;
+    private int RubbishCollected = 0;
+
+    private List<GameObject> binList = new List<GameObject>();
+
+    // Use this for initialization
+    void Start () {
+        foreach (Transform child in GameObject.Find("RubbishBins").transform)
+            binList.Add(child.gameObject);
+
+        canvas = GameObject.Find("Canvas");
+        WinPanel.SetActive(false);
+        LosePanel.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        CheckRubbishCount();
+
+        TimeElapsed += Time.deltaTime;
+
+        if (TimeElapsed > TimeLimit)
+        {
+            //if (  )
+        }
+
+    }
+
+    void CheckRubbishCount()
+    {
+        RubbishCollected = 0;
+
+        for (int i = 0; i < binList.Count; i++)
+            RubbishCollected += binList[i].GetComponent<BinCount>().objectCounts;
+    }
+
+}
