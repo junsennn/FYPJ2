8b89d87ce75b175b250763870faf93682a958aa6
junsennn <ongjunsen@gmail.com>
2017-01-09 01:40:31 +0800

    er... yeah jiu will have rubbish spawn , can set how many u want , set centre point and the scale :O yeap thats all NEYGA GG EZ

---
+++ Assets/Scripts/InstantiateRubbish.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstantiateRubbish : MonoBehaviour {
+
+    private GameObject rubbishPool;
+    private int poolSize;
+
+    public int RubbishAmount;
+
+    public Vector3 centrePoint;
+    public Vector3 scale;
+
+    // Use this for initialization
+    void Start () {
+        rubbishPool = GameObject.Find("RubbishPool");
+
+        foreach (Transform child in rubbishPool.transform)
+            poolSize += 1;
+
+        int currentAmount = 0;
+
+        while (currentAmount < RubbishAmount)
+        {
+            GameObject newRubbish = Instantiate<GameObject>(rubbishPool.transform.GetChild(Random.Range(0, poolSize)).gameObject);
+
+            newRubbish.transform.position = new Vector3(Random.Range(centrePoint.x - scale.x/2, centrePoint.x + scale.x / 2), centrePoint.y , Random.Range(centrePoint.z - scale.z / 2, centrePoint.z + scale.z / 2));
+            while(newRubbish.GetComponent<Rubbish_OG_pos>().cState == Rubbish_OG_pos.collisionState.Enter || newRubbish.GetComponent<Rubbish_OG_pos>().cState == Rubbish_OG_pos.collisionState.Stay)
+            {   
+                newRubbish.transform.position = new Vector3(Random.Range(centrePoint.x - scale.x / 2, centrePoint.x + scale.x / 2), centrePoint.y, Random.Range(centrePoint.z - scale.z / 2, centrePoint.z + scale.z / 2));
+            }
+
+            newRubbish.SetActive(true);
+
+            newRubbish.GetComponent<Rubbish_OG_pos>().SetOG(newRubbish.transform.position, newRubbish.transform.rotation);
+
+            currentAmount += 1;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Scripts/Rubbish_OG_pos.cs
+++ Assets/Scripts/Rubbish_OG_pos.cs
@@ -6,16 +6,43 @@ public class Rubbish_OG_pos : MonoBehaviour {
     public Vector3 OG_Pos;
     public Quaternion OG_Rotation;
 
+    public enum collisionState
+    {
+        None,
+        Enter,
+        Exit,
+        Stay,
+    };
+
+    public collisionState cState;
+
 	// Use this for initialization
 	void Start () {
         OG_Pos = transform.position;
         OG_Rotation = transform.rotation;
+
+        cState = collisionState.None;
     }
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    void OnCollisionEnter()
+    {
+        cState = collisionState.Enter;
+    }
+
+    void OnCollisionStay()
+    {
+        cState = collisionState.Stay;
+    }
+
+    void OnCollisionExit()
+    {
+        cState = collisionState.Exit;
+    }
 
     public void ResetToOG()
     {
