59a78d6db6b8c5355a203a450fad556f35329c0b
junsennn <ongjunsen@gmail.com>
2017-01-26 23:59:08 +0800

    crosshair color change but got bugs.... fix another day

--- Assets/Scripts/Weapon/ObjectGrab.cs
+++ Assets/Scripts/Weapon/ObjectGrab.cs
@@ -12,10 +12,16 @@ public class ObjectGrab : MonoBehaviour
     private float ShotPreventHold = 0.1f;
     public float ShotCD = 0.2f;
 
+    private GameObject redCrosshair;
+    private GameObject greenCrosshair;
+
     // Use this for initialization
     void Start()
     {
         ShotPreventHold = ShotCD;
+
+        redCrosshair = GameObject.Find("RedCrosshair");
+        greenCrosshair = GameObject.Find("GreenCrosshair");
     }
 
     // Update is called once per frame
@@ -38,17 +44,28 @@ public class ObjectGrab : MonoBehaviour
         #region Raycast 
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         //Debug.DrawRay(transform.position, ray.direction * 10);
-   
+
+        
+        greenCrosshair.SetActive(false);
+        redCrosshair.SetActive(true);
+        
+
         //Debug.DrawRay(ray.origin, ray.direction * 10, Color.cyan);
         if (Physics.Raycast(ray, out hit, distance))
         {
             //print("Im looking at " + hit.transform.name);
             if (hit.collider.tag == "Rubbish")
             {
+                if (redCrosshair.activeInHierarchy)
+                {
+                    redCrosshair.SetActive(false);
+                    greenCrosshair.SetActive(true);
+                }
+
                 //if(Input.GetKeyDown(KeyCode.E) && itemGrabbed == null)
                 if (Input.GetMouseButton(0) && itemGrabbed == null && !Shot)
                 {
-                    if(!hit.transform.gameObject.GetComponent<ObjectFollow>())
+                    if (!hit.transform.gameObject.GetComponent<ObjectFollow>())
                         hit.transform.gameObject.AddComponent<ObjectFollow>();
 
                     itemGrabbed = hit.transform.gameObject;
