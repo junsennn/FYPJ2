f29c99900569b3dc5d03ef7394f161e7c5ab3400
JinLimJim <143099Q@mymail.nyp.edu.sg>
2017-02-01 21:01:03 +0800

    Arrow Indicator, Billboard script
    -Added Arrow Indicator sprite animation
    -Added Billboard script

---
+++ Assets/Scripts/BillBoard.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BillBoard : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt (Camera.main.transform.position, Vector3.up);
+	}
+}
