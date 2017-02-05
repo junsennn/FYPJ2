0a14e397c7b10469b5779a0e84b3d615f1d263cb
JinLimJim <143099Q@mymail.nyp.edu.sg>
2017-02-05 16:28:47 +0800

    New Town Tutorial Scene
    New Town Tutorial Scene

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+	public GameObject[] Doors;
+	public float DoorMaxWidth;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator OpenDoor(int DoorNo)
+	{
+		while (Doors[DoorNo].transform.position.y < DoorMaxWidth)
+		{
+			Doors[DoorNo].transform.position = Vector3.Lerp(Doors[DoorNo].transform.position, new Vector3(Doors[DoorNo].transform.position.x, DoorMaxWidth , Doors[DoorNo].transform.position.z), 1 * Time.deltaTime );
+
+			yield return new WaitForEndOfFrame();
+		}
+
+	}
+}
