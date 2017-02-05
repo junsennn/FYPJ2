6f31e9918ccce5a7442480fb914414ef95237141
junsennn <ongjunsen@gmail.com>
2017-02-05 20:53:26 +0800

    tutorial stuff . good old hardcode style . LOLOLOL . the gates tho got 2 separate one then so mafan , got animator but got animation or not uh ? i nvr check.

--- Assets/Scripts/TutorialScripts/TutorialScript.cs
+++ Assets/Scripts/TutorialScripts/TutorialScript.cs
@@ -1,60 +1,59 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TutorialScript : MonoBehaviour {
+public class TutorialScript : MonoBehaviour
+{
 
     public GameObject[] RubbishBins;
     public GameObject[] Doors;
+    public GameObject[] DoorEndPos;
 
     public int[] RubbishCountNeeded;
 
-    public float DoorMaxHeight;
-
-    private int TutorialStage = 0;
+    private int TutorialStage = 1;
 
     private bool tutorialComplete = false;
 
-	// Use this for initialization
-	void Start () {
-	    
-	}
-	
-	// Update is called once per frame
-	void Update ()
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
     {
         if (!tutorialComplete)
         {
-            if (TutorialStage != 2)
-            {
-                if (RubbishBins[TutorialStage].GetComponent<BinCount>().objectCounts >= RubbishCountNeeded[TutorialStage])
-                    GoNextStage();
-            }
-            else
-            {
-                if (GameObject.Find("Player").transform.GetChild(0).GetComponent<ChangeWeapon>().weapon_Name.text == "Zarya Gun")
-                    GoNextStage();
-            }
+            if (RubbishBins[TutorialStage - 1].GetComponent<BinCount>().objectCounts >= RubbishCountNeeded[TutorialStage - 1])
+                GoNextStage();
         }
-        else
-        {
-            PlayerPrefs.SetInt("TutDone", 1);
-        }
-	}
+    }
 
     void GoNextStage()
     {
-        StartCoroutine(OpenDoor(TutorialStage));
+        StartCoroutine(OpenDoor(TutorialStage * 2));
         TutorialStage += 1;
 
-        if (TutorialStage >= RubbishCountNeeded.Length)
+        if (TutorialStage > 3)
+        {
             tutorialComplete = true;
+            PlayerPrefs.SetInt("TutDone", 1);
+        }
     }
 
     IEnumerator OpenDoor(int DoorNo)
     {
-        while (Doors[DoorNo].transform.position.y < DoorMaxHeight)
+        while ((Doors[DoorNo - 2].transform.position - DoorEndPos[DoorNo - 2].transform.position).magnitude > 0.001f &&
+           (Doors[DoorNo - 2].transform.eulerAngles - DoorEndPos[DoorNo - 2].transform.eulerAngles).magnitude > 0.001f &&
+           (Doors[DoorNo - 1].transform.position - DoorEndPos[DoorNo - 1].transform.position).magnitude > 0.001f &&
+           (Doors[DoorNo - 1].transform.eulerAngles - DoorEndPos[DoorNo - 1].transform.eulerAngles).magnitude > 0.001f)
         {
-            Doors[DoorNo].transform.position = Vector3.Lerp(Doors[DoorNo].transform.position, new Vector3(Doors[DoorNo].transform.position.x, DoorMaxHeight , Doors[DoorNo].transform.position.z), 1 * Time.deltaTime );
+            Doors[DoorNo - 2].transform.position = Vector3.Lerp(Doors[DoorNo - 2].transform.position, DoorEndPos[DoorNo - 2].transform.position, 1 * Time.deltaTime);
+            Doors[DoorNo - 2].transform.eulerAngles = Vector3.Lerp(Doors[DoorNo - 2].transform.eulerAngles, DoorEndPos[DoorNo - 2].transform.eulerAngles, 1 * Time.deltaTime);
+
+            Doors[DoorNo - 1].transform.position = Vector3.Lerp(Doors[DoorNo - 1].transform.position, DoorEndPos[DoorNo - 1].transform.position, 1 * Time.deltaTime);
+            Doors[DoorNo - 1].transform.eulerAngles = Vector3.Lerp(Doors[DoorNo - 1].transform.eulerAngles, DoorEndPos[DoorNo - 1].transform.eulerAngles, 1 * Time.deltaTime);
 
             yield return new WaitForEndOfFrame();
         }
